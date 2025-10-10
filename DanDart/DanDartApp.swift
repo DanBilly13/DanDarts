a47a39124ea72b51410309d3b35ece18e78b66e7
Dan Billingham <danbillingham@gmail.com>
2025-10-10 19:43:57 +0200

    Initial Commit

---
+++ DanDart/DanDartApp.swift
@@ -0,0 +1,17 @@
+//
+//  DanDartApp.swift
+//  DanDart
+//
+//  Created by Billingham Daniel on 2025-10-10.
+//
+
+import SwiftUI
+
+@main
+struct DanDartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DanDartTests/DanDartTests.swift
@@ -0,0 +1,17 @@
+//
+//  DanDartTests.swift
+//  DanDartTests
+//
+//  Created by Billingham Daniel on 2025-10-10.
+//
+
+import Testing
+@testable import DanDart
+
+struct DanDartTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
